1996fcff74f6e867bf3f93f674d5608c3aa8b04e
yotyot <82359786+yot-yot@users.noreply.github.com>
2024-06-14 14:58:17 +0900

    Update MapPlacing.cs

--- Project/Assets/MapPlacing.cs
+++ Project/Assets/MapPlacing.cs
@@ -5,42 +5,29 @@ public class MapPlacing : MonoBehaviour
 {
     private void Start()
     {
-
-        System.Random num = new System.Random();
+        System.Random num = new Random();
         string result = " ";
-        int[,] MapArr = new int[6, 6];
-
-
-
-        for (int i = 0; i < 6; i++)
-        {
-            for (int j = 0; j < 5; j++)
-            {
-                if (MapArr[i, j] != 4 || MapArr[i, j] != 3)
-                {
-                    MapArr[i, j] = num.Next(3);
-                }
+        int [,] MapArr = new int[6,6];
+        int i;
+        int j;
+        for(i=0; i<6; i++){
+            for(j=0; j<5; j++){
+                MapArr[i,j] = num.Next(3);
             }
         }
-
-        for (int i = 0; i < 6; i++)
-        {
-            //Console.WriteLine(num.Next(3));
-            MapArr[i, 3] = 4;
-            MapArr[i, 1] = 4;
+        for(i=0; i<6; i++){
+            MapArr[i,3] = 4; 
+            MapArr[i,1] = 4; 
         }
-        for (int i = 0; i < 6; i++)
-        {
-            MapArr[4, i] = 4;
+        for(i=0; i<6; i++){
+            MapArr[4,i] = 4; 
         }
-        MapArr[5, 2] = 4;
-        MapArr[5, 0] = 3;
-        MapArr[3, 2] = 5;
-        for (int i = 0; i < 6; i++)
-        {
-            for (int j = 0; j < 5; j++)
-            {
-                result = result + MapArr[i, j] + "  ";
+        MapArr[3,2] = 5;
+        MapArr[5,2] = 4; 
+        MapArr[5,0] = 3; 
+        for(i=0; i<6; i++){
+            for(j=0; j<5; j++){
+                result = result + MapArr[i,j] + "  ";
             }
             result = result + "\n ";
         }
