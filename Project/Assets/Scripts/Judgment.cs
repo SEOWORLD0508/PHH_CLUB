e68ce582ca904df63c0598b36a7546e499ced0cf
sin0530 <sinsung0530@gmail.com>
2024-07-19 15:29:29 +0900

    Update Judgment.cs

--- Project/Assets/Scripts/Judgment.cs
+++ Project/Assets/Scripts/Judgment.cs
@@ -1,5 +1,10 @@
 // 플레이어랑 몬스터 데미지 판정 구현
-
+bool P_attacked;
+bool E_attacked;
+if P_attacked == true
+{
+  
+}
 // 사거리랑 플레이어, 몬스터 사이의 거리를 바탕으로 함수 1개만 만들어서 플레이어랑 몬스터 둘다 같은 함수 사용
 
 // 회피 판정은 무적으로 만들어서 때움, playermovement에서 구현
