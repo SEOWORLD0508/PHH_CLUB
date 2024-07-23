4d051de798bace728780b5bdd0d2c030e6f4f489
aliceyang4932 <jini4932@gmail.com>
2024-07-24 03:03:03 +0900

    assest tile

---
+++ Project/Assets/2D_SD_Player/TAnimation_Monster.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TAnimation_Monster : MonoBehaviour
+{
+    Animator anim;
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (anim.GetBool("isAttack") == false)
+            {
+                anim.SetBool("isAttack", true);
+                anim.SetBool("isIdle", false);
+            }
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (anim.GetBool("isHit") == false)
+            {
+                anim.SetBool("isHit", true);
+                anim.SetBool("isIdle", false);
+            }
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            if (anim.GetBool("isDie") == false)
+            {
+                anim.SetBool("isDie", true);
+                anim.SetBool("isIdle", false);
+            }
+        }
+        else if (Input.GetKey(KeyCode.F))
+        {
+            if (anim.GetBool("isWalk") == false)
+            {
+                anim.SetBool("isWalk", true);
+                anim.SetBool("isIdle", false);
+            }
+        }
+        else
+        {
+            if (anim.GetBool("isIdle") == false)
+            {
+                anim.SetBool("isHit", false);
+                anim.SetBool("isIdle", true);
+                anim.SetBool("isAttack", false);
+                anim.SetBool("isDie", false);
+                anim.SetBool("isWalk", false);
+            }
+        }
+    }
+}
