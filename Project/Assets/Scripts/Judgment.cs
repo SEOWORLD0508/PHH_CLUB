43caf82bb0f92dd3f469ca88044030cb26675f46
sin0530 <sinsung0530@gmail.com>
2024-08-08 09:58:47 +0900

    Judgment.cs

--- Project/Assets/Scripts/Judgment.cs
+++ Project/Assets/Scripts/Judgment.cs
@@ -8,7 +8,7 @@ void update
 { 
   if (p_attacked == true)
   {
-    
+    currentHp-
   }
   if (e_attacked == true)
   {
@@ -18,4 +18,4 @@ void update
 // 사거리랑 플레이어, 몬스터 사이의 거리를 바탕으로 함수 1개만 만들어서 플레이어랑 몬스터 둘다 같은 함수 사용
 
 // 회피 판정은 무적으로 만들어서 때움, playermovement에서 구현
-*/
+
