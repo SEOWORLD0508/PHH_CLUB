3a1a28009ed4ecb6234aa46901c34aa11abb94af
ddm9490 <ddm9490@gmail.com>
2024-08-19 00:18:18 +0900

    상점

---
+++ Project/Assets/Scripts/Shop.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// NPC 코드
+
+public class Shopping : MonoBehaviour {
+    [SerializeField]
+    float interactionDistance;
+
+    [SerializeField]
+    public ItemHolder[] ItemList;
+    
+    void Setup() {
+       
+    }
+    void Start() {
+    }
+    void Update()
+    {
+        GameObject Player = FindObjectOfType<PlayerMovement>().gameObject;
+        
+        float dis = Vector3.Distance(Player.transform.position, transform.position);
+        if(dis < interactionDistance)
+        {
+            
+            if (Input.GetKeyDown(KeyCode.E)) // 인터렉션
+            {
+                print("interacted");
+            }
+           
+
+        }
+            
+
+
+    }
+        
+        
+    
+}
