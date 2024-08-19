94a8db129d3b4ccae96e253b3fad2689108a1fc7
eiekemeiekem <58236851+eiekemeiekem@users.noreply.github.com>
2024-08-19 10:32:14 +0900

    Update Judgment.cs

--- Project/Assets/Scripts/Judgment.cs
+++ Project/Assets/Scripts/Judgment.cs
@@ -2,18 +2,13 @@
 // 플레이어랑 몬스터 데미지 판정 구현
 // 공격자 클래스랑 피해자 클래스를 파라미터로 받아서 공격자와 피해자 사이의 거리를 계산하고, 공격자 클래스의 사정거리값을 기준으로 맞았는지 안맞았는지 판정한다. 
 // 몬스터와 플레이어 모두 피가 닳도록 하는 함수 이름을 통일하여 피해자 클래스의 체력 감소 함수를 불러와 데미지 판정을 구현
-bool p_attacked;
-bool e_attacked;
-void update
-{ 
-  if (p_attacked == true)
-  {
-    currentHp-
-  }
-  if (e_attacked == true)
-  {
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackEvent : MonoBehaviour{
     
-  }
 }
 // 사거리랑 플레이어, 몬스터 사이의 거리를 바탕으로 함수 1개만 만들어서 플레이어랑 몬스터 둘다 같은 함수 사용
 
