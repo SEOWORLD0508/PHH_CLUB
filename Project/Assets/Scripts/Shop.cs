d0dea531e75870502843f46a90a739ad0a965c53
klecton64 <166788106+klecton64@users.noreply.github.com>
2024-08-30 14:20:51 +0900

    shop ADDDED

--- Project/Assets/Scripts/Shop.cs
+++ Project/Assets/Scripts/Shop.cs
@@ -6,38 +6,41 @@ using UnityEngine;
 
 // NPC 코드
 
-public class Shopping : MonoBehaviour {
+public class Shopping : MonoBehaviour
+{
     [SerializeField]
     float interactionDistance;
 
     [SerializeField]
     public ItemHolder[] ItemList;
-    
-    void Setup() {
-       
+
+    void Setup()
+    {
+
     }
-    void Start() {
+    void Start()
+    {
     }
     void Update()
     {
         GameObject Player = FindObjectOfType<PlayerMovement>().gameObject;
-        
+
         float dis = Vector3.Distance(Player.transform.position, transform.position);
-        if(dis < interactionDistance)
+        if (dis < interactionDistance)
         {
-            
+
             if (Input.GetKeyDown(KeyCode.E)) // 인터렉션
             {
                 print("interacted");
             }
-           
+
 
         }
-            
+
 
 
     }
-        
-        
-    
+
+
+
 }
