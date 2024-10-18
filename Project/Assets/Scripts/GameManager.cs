f9fad3989eb4d4fa1c1f748bcef18706ab3145c4
klecton64 <166788106+klecton64@users.noreply.github.com>
2024-10-18 15:23:42 +0900

    Update GameManager.cs

--- Project/Assets/Scripts/GameManager.cs
+++ Project/Assets/Scripts/GameManager.cs
@@ -17,7 +17,7 @@ public class GameManager : MonoBehaviour
     public Transform DescriptionBase;
     public TMP_Text NameText;
     public TMP_Text DescriptionText;
-    public KeyCode InventoryKey = KeyCode.Tab
+    public KeyCode InventoryKey = KeyCode.Tab;
     private static GameManager _instance;
     [Header("Gold")]
     public float Gold=0;
