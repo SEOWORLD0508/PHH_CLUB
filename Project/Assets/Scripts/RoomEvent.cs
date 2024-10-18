1006267e03192be3edc2b7e7c0dd6fc6849f28ff
aliceyang4932 <jini4932@gmail.com>
2024-10-18 15:40:30 +0900

    eve

---
+++ Project/Assets/Scripts/RoomEvent.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomEvent : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //public delegate void KillEnemy(object sender, EventArgs e);
+}
