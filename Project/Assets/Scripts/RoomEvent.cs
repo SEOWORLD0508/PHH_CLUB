6732fc8ae7b4b0a931b8cae90988ae0516609793
yotyot <82359786+yot-yot@users.noreply.github.com>
2024-10-26 11:23:49 +0900

    Revert "room event part"
    This reverts commit c3161724575cfe7f42ae6f58cad599b69ffe87d2.

--- Project/Assets/Scripts/RoomEvent.cs
+++ Project/Assets/Scripts/RoomEvent.cs
@@ -4,34 +4,15 @@ using UnityEngine;
 
 public class RoomEvent : MonoBehaviour
 {
-    int Width = MapPlacing.instance.PblcWidth; //맵의 가로
-    int Height = MapPlacing.instance.PblcHeight; //맵의 세로
-    int i, j;
+    // Start is called before the first frame update
     void Start()
     {
-
+        
     }
+
+    // Update is called once per frame
     void Update()
     {
-        for (i = 0; i < Height; i++)
-        {
-            for (j = 0; j < Width; j++)
-            {
-                if (MapPlacing.instance.RoomInfo[i, j].isCleared == false && MapPlacing.instance.RoomInfo[i, j].EnemyAmount == 0)
-                {
-                    RoomClear(i, j);
-                }
-            }
-        }
-    }
-
-    public static void MinusEnemy(int y, int x) //적 수 차감
-    {
-        MapPlacing.instance.RoomInfo[y, x].EnemyAmount--;
-    }
-
-    public static void RoomClear(int y, int x) //방 클리어 선언
-    {
-        MapPlacing.instance.RoomInfo[y, x].isCleared = true;
+        
     }
 }
