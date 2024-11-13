a75068e7705896189bdf272a620450f8acb32b89
ddm9490 <ddm9490@gmail.com>
2024-11-13 20:31:35 +0900

    구매기능

---
+++ Project/Assets/Scripts/Shop/ItemBuyEvent.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemBuyEvent : MonoBehaviour
+{
+    public ItemPrefab saleItemPrefab;
+    // Start is called before the first frame update
+    
+    public void OnClickBuyItemButton()
+    {
+        saleItemPrefab = transform.parent.GetComponent<ItemPrefab>();
+        print(saleItemPrefab.amount);
+        print(saleItemPrefab.item.name);
+        /*
+        saleItemPrefab.amount -= 1;
+        if (saleItemPrefab.amount <= 0)
+        {
+            disableBuyButton();
+        }
+        */
+        
+    }
+
+    void disableBuyButton()
+    {
+
+    }
+}
